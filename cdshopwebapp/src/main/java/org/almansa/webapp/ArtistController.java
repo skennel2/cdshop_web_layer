94939ed941b6ad996bde06a965c37f658178a536
skennel2 <skennel2@gmail.com>
2018-06-04 23:54:02 +0900

    Add ArtistController

---
+++ cdshopwebapp/src/main/java/org/almansa/webapp/ArtistController.java
@@ -0,0 +1,14 @@
+package org.almansa.webapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/artist")
+public class ArtistController {
+    
+    //@Autowired
+    
+    
+}
--- cdshopwebapp/src/main/java/org/almansa/webapp/WebUserController.java
+++ cdshopwebapp/src/main/java/org/almansa/webapp/WebUserController.java
@@ -1,5 +1,7 @@
 package org.almansa.webapp;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
 
 import org.almansa.app.domain.user.ApplicationUser;
@@ -23,7 +25,8 @@ public class WebUserController {
     
     @RequestMapping(path="/add", method=RequestMethod.POST) 
     public void joinMember(@ModelAttribute UserJoinRequest request) { 
-        // @ModelAttribute는 HTTP 바디에 x-www-form-urlencoded로 던져야 바인딩된다
+        // @ModelAttribute는 HTTP 바디에 Content-Type/x-www-form-urlencoded로 던져야 바인딩된다
+        // Content-Type/x-www-form-urlencoded는 key/value로 값을 던져야하는 규약
         if(request == null) {
             throw new IllegalArgumentException("UserJoinRequest can't be null");
         }
@@ -39,7 +42,7 @@ public class WebUserController {
     @RequestMapping(path="/login", method=RequestMethod.POST)
     public void login(@RequestParam String loginId, @RequestParam String password, HttpSession session) {
         if(userService.isAbleToLogin(loginId, password)) {
-            session.setAttribute("login", new LoginSessionModel());
+            session.setAttribute("login", new LoginSessionModel(loginId, new Date()));
         }
     }
 }
