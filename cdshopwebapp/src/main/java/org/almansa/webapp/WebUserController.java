8b49208c42a751591001c02f4c8a78d2048d6098
나윤수 <skennel2@gmail.com>
2018-06-08 14:31:49 +0900

    source cleanup

--- cdshopwebapp/src/main/java/org/almansa/webapp/WebUserController.java
+++ cdshopwebapp/src/main/java/org/almansa/webapp/WebUserController.java
@@ -1,13 +1,11 @@
 package org.almansa.webapp;
 
-import java.util.Date;
-
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 import org.almansa.app.domain.user.ApplicationUser;
 import org.almansa.app.service.ApplicationUserService;
 import org.almansa.app.service.dto.UserJoinRequest;
-import org.almansa.webapp.dto.LoginSessionModel;
+import org.almansa.app.service.exception.ApplicationUserJoinException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,31 +23,27 @@ public class WebUserController {
     ApplicationUserService userService;
     
     @RequestMapping(path="/add", method=RequestMethod.POST) 
-    public void joinMember(@ModelAttribute UserJoinRequest request) { 
-        // @ModelAttribute는 HTTP 바디에 Content-Type/x-www-form-urlencoded로 던져야 바인딩된다
-        // Content-Type/x-www-form-urlencoded는 key/value로 값을 던져야하는 규약
-        if(request == null) {
-            throw new IllegalArgumentException("UserJoinRequest can't be null");
+    public ResponseEntity<String> joinMember(@ModelAttribute UserJoinRequest request) { 
+        try {
+        	userService.joinUser(request);
+        	
+        	return new ResponseEntity<String>(HttpStatus.OK);        	        	
+        }catch(ApplicationUserJoinException e) {        	
+        	return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         }
-        
-        userService.joinUser(request);
     }
     
     @RequestMapping(path="/get/{loginId}")
     public ResponseEntity<ApplicationUser> getByLoginId(@PathVariable String loginId) {
         ApplicationUser user = userService.getUserByLoginId(loginId);
         
-        if(user == null) {
-            return new ResponseEntity<ApplicationUser>(HttpStatus.NO_CONTENT);
-        }
-        
         return new ResponseEntity<ApplicationUser>(user, HttpStatus.OK);
     }
     
-    @RequestMapping(path="/login", method=RequestMethod.POST)
-    public void login(@RequestParam String loginId, @RequestParam String password, HttpSession session) {
-        if(userService.isAbleToLogin(loginId, password)) {
-            session.setAttribute("login", new LoginSessionModel(loginId, new Date()));
-        }
-    }
+    @RequestMapping(path="/get")
+    public ResponseEntity<List<ApplicationUser>> getAllUser() {
+    	List<ApplicationUser> user = userService.findAll();
+        
+        return new ResponseEntity<List<ApplicationUser>>(user, HttpStatus.OK);
+    }    
 }
